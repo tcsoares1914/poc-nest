38cbbecab465ad95e7ce1ba7daf04d08ff472d4b
Thiago Soares <tcsoares84@gmail.com>
2022-07-12 20:10:41 -0300

    Intall husku + lint-staged.

---
+++ src/interfaces/config.ts
@@ -0,0 +1,10 @@
+export interface ApiConfig {
+  /**
+   * Port of app are running.
+   */
+  port: number
+  /**
+   * Exposed port of app.
+   */
+  exposedPort: number
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config'
+import { Logger, ValidationPipe } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true })
+  app.useGlobalPipes(new ValidationPipe())
+  const configService = app.get(ConfigService)
+  const port = configService.get('api.port')
+  const exposedPort = configService.get('api.exposedPort')
+  await app.listen(port)
+  Logger.log(
+    `🔥 Nest Server running on http://localhost:${exposedPort}`,
+    'NestBootstrap',
+  )
 }
-bootstrap();
+bootstrap()
