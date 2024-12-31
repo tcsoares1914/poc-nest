2ceaf619ed4cdc585cff54c8f9307eb22c9675de
Thiago Soares <tcsoares84@gmail.com>
2024-12-30 23:11:52 -0300

    fix: unit tests dependencies
    - fix unit tests dependencies for run tests and pass;

--- src/certifications/tests/certifications.resolver.spec.ts
+++ src/certifications/tests/certifications.resolver.spec.ts
@@ -1,13 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { CertificationsResolver } from '../certifications.resolver';
-import { CertificationsService } from '../certifications.service';
+import { CertificationsResolver } from '@src/certifications/certifications.resolver';
+import { CertificationsService } from '@src/certifications/certifications.service';
 
 describe('CertificationsResolver', () => {
   let resolver: CertificationsResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CertificationsResolver, CertificationsService],
+      providers: [
+        CertificationsResolver,
+        {
+          provide: CertificationsService,
+          useValue: {
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            create: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     resolver = module.get<CertificationsResolver>(CertificationsResolver);
--- src/certifications/tests/certifications.service.spec.ts
+++ src/certifications/tests/certifications.service.spec.ts
@@ -1,12 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { CertificationsService } from '../certifications.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { CertificationsService } from '@src/certifications/certifications.service';
+import { Certification } from '@src/certifications/schemas/certification.schema';
 
 describe('CertificationsService', () => {
   let service: CertificationsService;
 
+  const mockModel = {
+    save: jest.fn(),
+    find: jest.fn(),
+    findById: jest.fn(),
+    findByIdAndUpdate: jest.fn(),
+    findByIdAndDelete: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CertificationsService],
+      providers: [
+        {
+          provide: getModelToken(Certification.name),
+          useValue: mockModel,
+        },
+        CertificationsService,
+      ],
     }).compile();
 
     service = module.get<CertificationsService>(CertificationsService);
